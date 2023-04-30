1dd46f5cd09d86f94b7326ac917c64db6d994384
wouldu <jhw664089@gmail.com>
2023-04-30 20:03:26 +0900

    add image

--- instance/app/portfolio/bestshot/page.js
+++ instance/app/portfolio/bestshot/page.js
@@ -1,7 +1,25 @@
+import Image from "next/image";
+import team_img from '/public/dev.jpg'
+
 export default function SKTproject() {
+    let team = ['Frontend Developer', 'Backend Developer', 'Machine Learning Engineer']
+    let team_img = ['/dev.jpg', '/dev.jpg', '/dev.jpg']
+
+    
+
     return (
         <div>
             <h1>SKTproject</h1>
+            <div>
+                {team.map((item, index) => {
+                    return (
+                        <div className="project" key={index}>
+                            <img src={team_img[index]} style={{ width: "50%", height: "50%", objectFit: "cover" }} />
+                            <h4>{item}</h4>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
